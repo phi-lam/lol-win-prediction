e2546fc7cf602ce4a1db4f605ce52cfe7506c017
Phi (Macbook) <phi.lam64@gmail.com>
2018-03-15 01:19:50 -0700

    working: removing nan rows

--- lol-win-prediction.py
+++ lol-win-prediction.py
@@ -11,7 +11,9 @@ from operator import itemgetter
 from sklearn import svm
 from sklearn import linear_model
 from sklearn import discriminant_analysis
+from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
+
 
 ########################## VARIABLE DECLARATIONS ###########################
 
@@ -55,6 +57,35 @@ def parse_leagueoflegends(in_file):
 	df.to_csv('parsed_leagueoflegends.csv')
 	return out_array
 
+#
+#	FUNCTION: delete_nanrows(nparray_)
+#
+#	Description: Call this function after compiling all columns for the test.
+#
+def delete_nanrows(np_data, np_labels):
+	np_data = np.asarray(np_data)
+	np_labels = np.asarray(np_labels)
+	if len(np_data) != len(np_labels):
+		raise Exception("np_data and np_labels must have identical lengths")
+	if len(np_data) != num_instances:
+		raise Exception("delete_nanrows needs np_data with original num_instances")
+
+	#print("array lengths with nan")
+	#print("np_data: ", len(np_data), " | np_labels: ", len(np_labels))
+
+	np_labels = np_labels[~np.isnan(np_data).any(axis=1)]
+	np_data = np_data[~np.isnan(np_data).any(axis=1)]
+
+	#print("array lengths w/o nan")
+	#print("np_data: ", len(np_data), " | np_labels: ", len(np_labels))
+
+	np.savetxt("np_data.csv", np_data)
+	np.savetxt("np_labels.csv", np_labels)
+
+	#print("### np_data: ", np_data, " \n ### np_labels: ", np_labels)
+
+	return {'data': np_data, 'labels': np_labels}
+
 #-------------------
 #	FUNCTION: create_training_data()
 #
@@ -93,26 +124,28 @@ def parse_leagueoflegends(in_file):
 # 	print("Note: if out_file.txt missing, will print to stdout")
 # 	exit()
 
-# ---- Take input data ----
-
+# ==== Workflow:
+#	1) Parse all files
+#		- make a dict with columns from relevant timestamps
+#	2) For each timestamp (5_min, 10_min, 15_min, etc):
+#		- Assemble data set with columns from relevant timestamps (delete nan arrays)
+#		- Separate training and test data (train_test_split)
+#		- Train each model
+#		- Test each model
 
 print("Parsing gold.csv...")
 gold_nparray = parse_goldfile(gold_inputfile)
+num_instances = len(gold_nparray)
 
-d = {}
-d[0] = gold_nparray[:,4].reshape(-1,1) #gold_5min
-d[1] = gold_nparray[:,9].reshape(-1,1) #gold_10min
-d[2] = gold_nparray[:,14].reshape(-1,1) #gold_15min
-d[3] = gold_nparray[:,20].reshape(-1,1) #gold_20min
-d[4] = gold_nparray[:,25].reshape(-1,1) #gold_25min
-d[5] = gold_nparray[:,29].reshape(-1,1) #gold_30min
+gold_col = {}
+gold_col[0] = gold_nparray[:,4].reshape(-1,1) #gold_5min
+gold_col[1] = gold_nparray[:,9].reshape(-1,1) #gold_10min
+gold_col[2] = gold_nparray[:,14].reshape(-1,1) #gold_15min
+gold_col[3] = gold_nparray[:,20].reshape(-1,1) #gold_20min
+gold_col[4] = gold_nparray[:,25].reshape(-1,1) #gold_25min
+gold_col[5] = gold_nparray[:,29].reshape(-1,1) #gold_30min
+
 print("Done.")
-
-print("gold@20 with nan")
-print(d[3])
-print("gold@30 without nan")
-d[3] = d[3].dropna()
-print(d[3])
 
 print("Parsing LeagueofLegends.csv...")
 matchoutcomes_nparray = parse_leagueoflegends(leagueoflegends_inputfile)
@@ -120,49 +153,37 @@ print("Done.")
 
 #crossvalidation
 
-print("\n=== LDA, Gold Test === ")
 for i in range(6):
-	if np.isnan(d[i]).any():
-		continue
-	print(d[i])
-	gold_train, gold_test, outcome_train, outcome_test = train_test_split(d[i], matchoutcomes_nparray, test_size=0.2)
+	print("\n===== Prediction error at time: ", 5*(i + 1), ":00 =====", sep='')
 
-	clf1 = discriminant_analysis.LinearDiscriminantAnalysis()
-	clf1.fit(gold_train, outcome_train.ravel())
-	pred = clf1.predict(gold_test)
-	print("Prediction error at time: ", 5*(i + 1), ":00")
+	if np.isnan(gold_col[i]).any():
+		result = delete_nanrows(gold_col[i], matchoutcomes_nparray)
+		data = result['data']
+		outcomes = result['labels']
+	else:
+		data = gold_col[i]
+		outcomes = matchoutcomes_nparray
 
-	print("\t", clf1.score(gold_test, outcome_test), "\n")
+	#print("array lengths: ", len(data), len(outcomes))
+	data_train, data_test, outcome_train, outcome_test = train_test_split(data, outcomes, test_size=0.2)
 
-print("\n=== Logistic Regression, Gold Test === ")
-for i in range(6):
-	if np.isnan(d[i]).any():
-		continue
-	print(d[i])
-	gold_train, gold_test, outcome_train, outcome_test = train_test_split(d[i], matchoutcomes_nparray, test_size=0.2)
-	if np.isnan(gold_train).any():
-		continue
+	print("\n - LDA, Gold Test ")
+	lda = discriminant_analysis.LinearDiscriminantAnalysis()
+	lda.fit(data_train, outcome_train.ravel())
+	pred = lda.predict(data_test)
+	print("\t", lda.score(data_test, outcome_test), "\n")
 
-	clf2 = linear_model.LogisticRegression()
-	clf2.fit(gold_train, outcome_train.ravel())
-	pred = clf2.predict(gold_test)
+	print("\n - Logistic Regression, Gold Test")
+	log_reg = linear_model.LogisticRegression()
+	log_reg.fit(data_train, outcome_train.ravel())
+	pred = log_reg.predict(data_test)
+	print("\t", log_reg.score(data_test, outcome_test), "\n")
+	#print("\t", accuracy_score(outcome_test, pred), "\n")
 
-	print("Prediction error at time: ", 5*(i + 1), ":00")
-	print("\t", clf2.score(gold_test, outcome_test), "\n")
-
-
-print("\n=== SVM Classification, Gold Test === ")
-
-for i in range(6):
-	print(d[i])
-	gold_train, gold_test, outcome_train, outcome_test = train_test_split(d[i], matchoutcomes_nparray, test_size=0.2)
-	if np.isnan(gold_train).any():
-		continue
-
-	clf3 = svm.LinearSVC()
-	clf3.fit(gold_train, outcome_train.ravel())
-	pred = clf3.predict(gold_test)
-	print("Prediction error at time: ", 5*(i + 1), ":00")
-	print("\t", clf3.score(gold_test, outcome_test), "\n")
+	print("\n - SVM Classification, Gold Test")
+	svm_lin = svm.LinearSVC()
+	svm_lin.fit(data_train, outcome_train.ravel())
+	pred = svm_lin.predict(data_test)
+	print("\t", svm_lin.score(data_test, outcome_test), "\n")
 
 #end
